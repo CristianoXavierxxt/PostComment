e4221bbc9e58d3d51c6fb1e2a219ce47513ce310
CristianoXavierxxt <cristianox260@gmail.com>
2023-09-17 00:15:43 -0300

    Finalizado a estrutura da rede social

--- PostComment/src/application/Application.java
+++ PostComment/src/application/Application.java
@@ -1,19 +1,48 @@
 package application;
 
-import java.util.Locale;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+
+import entities.Comment;
+import entities.Post;
 
 public class Application {
 
-	public static void main(String[] args) {
-		Locale.setDefault( Locale.US );
+	public static void main(String[] args) throws ParseException {
+		
 		Scanner sc = new Scanner( System.in );
+		
+		SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );
 
+		Comment comment1 = new Comment( "Have a nice trip!" );
+		
+		Comment comment2 = new Comment( "Wow that's awesome!" );
+		
+		Post post1 = new Post( sdf.parse( "21/06/2018 13:05:44" ), 
+				"Traveling to new zealand", "I'm going to visit this wonderfull country!", 12 );
+		
+		post1.addComment(comment1);
+		post1.addComment(comment2);
 		
 		
 		
+		Comment comment3 = new Comment( "Good night" );
+		
+		Comment comment4 = new Comment( "May the force be with you" );
+		
+		Post post2 = new Post( sdf.parse( "28/07/2018 23:14:19" ), 
+				"Good night guys", "See you tomorrow!", 5 );
+		
+		post2.addComment(comment3);
+		post2.addComment(comment4);
 		
 		
+		System.out.println(post1);
+		
+		System.out.println();
+		
+		System.out.println(post2);
 		
 		sc.close();
 	}
--- PostComment/src/entities/Post.java
+++ PostComment/src/entities/Post.java
@@ -1,15 +1,20 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Post {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );
 
 	private Date moment;
 	private String title;
 	private String content;
 	private Integer likes;
 	
-	private Comment comment;
+	private List<Comment> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -54,16 +59,36 @@ public class Post {
 		this.likes = likes;
 	}
 
-	public Comment getComment() {
-		return comment;
+	public List<Comment> getComments() {
+		return comments;
 	}
 
-	public void setComment( Comment comment ) {
-		this.comment = comment;
+	
+	public void addComment( Comment comment ) {
+		comments.add( comment );
 	}
 	
+	public void removeComment( Comment comment ) {
+		comments.remove( comment );
+	}
 	
-	
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append( title + "\n" );
+		sb.append( likes );
+		sb.append( " Likes - " );
+		sb.append( sdf.format( moment ) + "\n" );
+		sb.append( content + "\n" );
+		sb.append( "Comments:\n" );
+		
+		for( Comment c : comments ) {
+			sb.append( c.getText() + "\n" );
+		}
+		
+		return sb.toString();
+	}
 	
 	
 }
